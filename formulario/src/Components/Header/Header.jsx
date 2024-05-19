446e5b0935524a049e34761e65bf156dbd1c8842
augustoccbraga <augustoccb.br@gmail.com>
2024-05-19 19:36:14 -0300

    add retorno para o site pessoal projeto formulario FinanSee 1

--- formulario/src/Components/Header/Header.jsx
+++ formulario/src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Return from "../Return/Return";
 import styles from "./Header.module.css";
 import logo from "../img/logo-finansee.svg";
 
 const Header = () => {
 	return (
 		<div className={styles.header}>
+			<Return />
 			<img src={logo} alt="Paytour" />
 		</div>
 	);
---
+++ formulario/src/Components/Return/Return.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Return.module.css";
+
+const Return = () => {
+	return (
+		<nav>
+			<a href="../index.html" className={styles.return}></a>
+		</nav>
+	);
+};
+
+export default Return;
